c3022d657440b4605b8359869d23bb668dbdf1d5
Michael Hubbard <michael.hubbard999@gmail.com>
2022-09-23 18:00:58 +0000

    Added a filename parameter
    Now you must specify the filename.
    This change was made to deal with multi-site customers.

--- dhcp-csv.ps1
+++ dhcp-csv.ps1
@@ -1,10 +1,10 @@
-param([string]$server = "server", [string]$scope = "scope")
-$a = Import-Csv DHCP.csv
+param ([string] $server = "server", [string]$scope = "scope", [string]$filename = "filename")
+$a = Import-Csv $filename
 foreach ($item in $a) {
 $ip=$($item.IP)
 $mac=$($item.MAC)
 #remove colons since MS DHCP can't deal with a real mac address
 $mac=$mac-replace'[:]'
-$name = $($item."AP-Name")
+$name = $($item."Camera Lables")
 write-host "netsh dhcp server $server scope $scope add reservedip $ip $mac $name"
 }
