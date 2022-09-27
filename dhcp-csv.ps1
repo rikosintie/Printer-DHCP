0dd7b9b7b235ffff88b528b84e18bffbfac92994
Michael Hubbard <michael.hubbard999@gmail.com>
2022-09-27 22:18:50 +0000

    Added filename param

--- dhcp-csv.ps1
+++ dhcp-csv.ps1
@@ -1,10 +1,10 @@
 param ([string] $server = "server", [string]$scope = "scope", [string]$filename = "filename")
 $a = Import-Csv $filename
 foreach ($item in $a) {
-$ip=$($item.IP)
+$ip=$($item."IP Address")
 $mac=$($item.MAC)
 #remove colons since MS DHCP can't deal with a real mac address
 $mac=$mac-replace'[:]'
-$name = $($item."Camera Lables")
+$name = $($item."Camera Labels")
 write-host "netsh dhcp server $server scope $scope add reservedip $ip $mac $name"
 }
