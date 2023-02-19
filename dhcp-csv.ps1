84585e2cea169ef3e6f7ffacd1fff95f1c729eff
Michael Hubbard <michael.hubbard999@gmail.com>
2023-02-18 17:14:52 -0800

    Added line to remove "-" from MAC address

--- dhcp-csv.ps1
+++ dhcp-csv.ps1
@@ -1,10 +1,14 @@
-param ([string] $server = "server", [string]$scope = "scope", [string]$filename = "filename")
+param ([string]$filename = "filename")
 $a = Import-Csv $filename
 foreach ($item in $a) {
 $ip=$($item."IP Address")
 $mac=$($item.MAC)
-#remove colons since MS DHCP can't deal with a real mac address
+$server =$($item.DHCP_server)
+$scope=$($item.DHCP_scope)
+#remove colons/dashes since MS DHCP can't deal with a real mac address
 $mac=$mac-replace'[:]'
+$mac=$mac-replace'[-]'
 $name = $($item."Camera Labels")
-write-host "netsh dhcp server $server scope $scope add reservedip $ip $mac $name"
+$description = $($item."Camera location")
+write-host "netsh dhcp server $server scope $scope add reservedip $ip $mac $name $description"
 }
